e6891db2e1a704bc67699b003326983f96b2e1bd
Jacob Taylor <jacob.taylor0299@gmail.com>
2020-12-10 16:36:05 -0700

    testing images on server

--- front-end/src/songs-data.js
+++ front-end/src/songs-data.js
@@ -4,119 +4,119 @@ let songsList = [
   name: "Space Ghost Coast To Coast",
   artist: "Glass Animals",
   genre: "Anti Pop",
-  image: '/images/dreamland.png'
+  image: require('../images/dreamland.png')
 },
 {
   rank: 2,
   name: "IMPATIENT",
   artist: "SEBASTIAN PAUL",
   genre: "Anti Pop",
-  image: '/images/impatient.jpg'
+  image: require('../images/impatient.jpg')
 },
 {
   rank: 3,
   name: "Goliath",
   artist: "Woodkid",
   genre: "Chamber Pop",
-  image: '/images/s16.jpg'
+  image: require('../images/s16.jpg')
 },
 {
   rank: 4,
   name: "Babydoll",
   artist: "Dominic Fike",
   genre: "Alternative Hip Hop",
-  image: '/images/3nights.webp'
+  image: require('../images/3nights.webp')
 },
 {
   rank: 5,
   name: "CRUSH",
   artist: "SEBASTIAN PAUL",
   genre: "Anti Pop",
-  image: '/images/crush.jpg'
+  image: require('../images/crush.jpg')
 },
 {
   rank: 6,
   name: "Rollerblades",
   artist: "Dominic Fike",
   genre: "Alternative",
-  image: '/images/rollerblades.jpg'
+  image: require('../images/rollerblades.jpg')
 },
 {
   rank: 7,
   name: "going out",
   artist: "ROLE MODEL",
   genre: "Anti Pop",
-  image: '/images/rolemodel.jpeg'
+  image: require('../images/rolemodel.jpeg')
 },
 {
   rank: 8,
   name: "Horizons Into Battlegrounds",
   artist: "Woodkid",
   genre: "Art Pop",
-  image: '/images/s16.jpg'
+  image: require('../images/s16.jpg')
 },
 {
   rank: 9,
   name: "Heat Waves",
   artist: "Glass Animals",
   genre: "Anti Pop",
-  image: '/images/dreamland.png'
+  image: require('../images/dreamland.png')
 },
 {
   rank: 10,
   name: "Politics & Violence",
   artist: "Dominic Fike",
   genre: "Alternative",
-  image: '/images/wcpgw.jpg'
+  image: require('../images/wcpgw.jpg')
 },
 {
   rank: 11,
   name: "say it over (feat. Cautious Clay)",
   artist: "Ruel",
   genre: "Pop",
-  image: '/images/sayitover.jpg'
+  image: require('../images/sayitover.jpg')
 },
 {
   rank: 12,
   name: "Confidant",
   artist: "BabyJake",
   genre: "Alternative",
-  image: '/images/babyjake.jpg'
+  image: require('../images/babyjake.jpg')
 },
 {
   rank: 13,
   name: "Vampire",
   artist: "Dominic Fike",
   genre: "Alternative",
-  image: '/images/wcpgw.jpg'
+  image: require('../images/wcpgw.jpg')
 },
 {
   rank: 14,
   name: "FOOL'S GOLD",
   artist: "Aries",
   genre: "Anti Pop",
-  image: '/images/foolsgold.jpg'
+  image: require('../images/foolsgold.jpg')
 },
 {
   rank: 15,
   name: "paper thin",
   artist: "Christian French",
   genre: "Alternative Pop",
-  image: '/images/paperthin.jpg'
+  image: require('../images/paperthin.jpg')
 },
 {
   rank: 16,
   name: "blind",
   artist: "ROLE MODEL",
   genre: "Anti Pop",
-  image: '/images/rolemodel.jpeg'
+  image: require('../images/rolemodel.jpeg')
 },
 {
   rank: 17,
   name: "Better",
   artist: "The Vamps",
   genre: "Alternative Pop",
-  image: '/images/better.jpg'
+  image: require('../images/better.jpg')
 
 },
 {
@@ -124,21 +124,21 @@ let songsList = [
   name: "imma",
   artist: "bbno$ and lentra",
   genre: "Hip Hop",
-  image: '/images/imma.webp'
+  image: require('../images/imma.webp')
 },
 {
   rank: 19,
   name: "Tangerine",
   artist: "Glass Animals",
   genre: "Anti Pop",
-  image: '/images/dreamland.png'
+  image: require('../images/dreamland.png')
 },
 {
   rank: 20,
   name: "BACK AND FORTH",
   artist: "SEBASTIAN PAUL",
   genre: "Anti Pop",
-  image: '/images/backandforth.jpeg'
+  image: require('../images/backandforth.jpeg')
 
 },
 {
@@ -146,28 +146,28 @@ let songsList = [
   name: "good things take time",
   artist: "Christian French",
   genre: "Alternative Pop",
-  image: '/images/goodthings.jpg'
+  image: require('../images/goodthings.jpg')
 },
 {
   rank: 22,
   name: "I'm Not Mad",
   artist: "Halsey",
   genre: "Pop",
-  image: '/images/manic.png'
+  image: require('../images/manic.png')
 },
 {
   rank: 23,
   name: "Twenty Something",
   artist: "Nightly",
   genre: "Alternative Pop",
-  image: '/images/twenty.jpg'
+  image: require('../images/twenty.jpg')
 },
 {
   rank: 24,
   name: "Sadderdaze",
   artist: "The Neighbourhood",
   genre: "Alternative",
-  image: '/images/nb.jpg'
+  image: require('../images/nb.jpg')
 
 },
 {
@@ -175,182 +175,182 @@ let songsList = [
   name: "In Your Likeness",
   artist: "Woodkid",
   genre: "Chamber Pop",
-  image: '/images/s16.jpg'
+  image: require('../images/s16.jpg')
 },
 {
   rank: 26,
   name: "Bad Thoughts",
   artist: "bbno$",
   genre: "Alternative Hip Hop",
-  image: '/images/gone.jpg'
+  image: require('../images/gone.jpg')
 },
 {
   rank: 27,
   name: "Where's My Love",
   artist: "SYML",
   genre: "Folk/Indie",
-  image: '/images/syml.jpg'
+  image: require('../images/syml.jpg')
 },
 {
   rank: 28,
   name: "Phone Numbers",
   artist: "Dominic Fike",
   genre: "Alternative Hip Hop",
-  image: '/images/phonenumbers.jpg'
+  image: require('../images/phonenumbers.jpg')
 },
 {
   rank: 29,
   name: "Daylight",
   artist: "Joji and Diplo",
   genre: "Dark Pop",
-  image: '/images/diplo.png'
+  image: require('../images/diplo.png')
 },
 {
   rank: 30,
   name: "for the people in the back",
   artist: "ROLE MODEL",
   genre: "Alternative/Dark Pop",
-  image: '/images/rolemodel.jpeg'
+  image: require('../images/rolemodel.jpeg')
 },
 {
   rank: 31,
   name: "Gone",
   artist: "bbno$",
   genre: "Hip Hop/Dark Pop",
-  image: '/images/gone.jpg'
+  image: require('../images/gone.jpg')
 },
 {
   rank: 32,
   name: "thankful",
   artist: "bbno$",
   genre: "Pop/Hip Hop",
-  image: '/images/bbno.jpg'
+  image: require('../images/bbno.jpg')
 },
 {
   rank: 33,
   name: "TROJAN HORSE",
   artist: "SEBASTIAN PAUL",
   genre: "Anti Pop",
-  image: '/images/trojan.jpg'
+  image: require('../images/trojan.jpg')
 },
 {
   rank: 34,
   name: "Supercuts",
   artist: "Jeremy Zucker",
   genre: "Indie",
-  image: '/images/supercuts.jpg'
+  image: require('../images/supercuts.jpg')
 },
 {
   rank: 35,
   name: "i think too much",
   artist: "Christian French",
   genre: "Alternative Pop",
-  image: '/images/think.jpg'
+  image: require('../images/think.jpg')
 },
 {
   rank: 36,
   name: "Wurli",
   artist: "Dominic Fike",
   genre: "Alternative",
-  image: '/images/wcpgw.jpg'
+  image: require('../images/wcpgw.jpg')
 },
 {
   rank: 37,
   name: "Tokyo Drifting (with Denzel Curry)",
   artist: "Glass Animals",
   genre: "Anti Pop/Hip Hop",
-  image: '/images/tokyo.jpg'
+  image: require('../images/tokyo.jpg')
 },
 {
   rank: 38,
   name: "Angela",
   artist: "The Lumineers",
   genre: "Folk/Guitar",
-  image: '/images/angela.jpg'
+  image: require('../images/angela.jpg')
 },
 {
   rank: 39,
   name: "Talk",
   artist: "Hozier",
   genre: "Dark Indie/Folk",
-  image: '/images/talk.jpg'
+  image: require('../images/talk.jpg')
 },
 {
   rank: 40,
   name: "The Less I Know The Better",
   artist: "Tame Impala",
   genre: "Alternative",
-  image: '/images/less.png'
+  image: require('../images/less.png')
 },
 {
   rank: 41,
   name: "Head In The Clouds",
   artist: "BabyJake",
   genre: "Alternative",
-  image: '/images/babyjake.jpg'
+  image: require('../images/babyjake.jpg')
 },
 {
   rank: 42,
   name: "as long as you care",
   artist: "Ruel",
   genre: "Alternative Pop",
-  image: '/images/aslongas.jpg'
+  image: require('../images/aslongas.jpg')
 },
 {
   rank: 43,
   name: "nevermind",
   artist: "Valley",
   genre: "Alternative",
-  image: '/images/valley.jpg'
+  image: require('../images/valley.jpg')
 },
 {
   rank: 44,
   name: "cowboy in LA",
   artist: "LANY",
   genre: "Alternative",
-  image: '/images/lany.jpg'
+  image: require('../images/lany.jpg')
 },
 {
   rank: 45,
   name: "Hate The Way (feat. blackbear)",
   artist: "G-Eazy and blackbear",
   genre: "Hip Hop/Pop",
-  image: '/images/hatetheway.png'
+  image: require('../images/hatetheway.png')
 },
 {
   rank: 46,
   name: "Your Man",
   artist: "Joji",
   genre: "Dark Pop",
-  image: '/images/joji.jpg'
+  image: require('../images/joji.jpg')
 },
 {
   rank: 47,
   name: "Cigarettes On Patios",
   artist: "BabyJake",
   genre: "Alternative Pop",
-  image: '/images/cigs.jpg'
+  image: require('../images/cigs.jpg')
 },
 {
   rank: 48,
   name: "Exist",
   artist: "Phoebe Ryan",
   genre: "Alternative Pop",
-  image: '/images/phoebe.jpg'
+  image: require('../images/phoebe.jpg')
 },
 {
   rank: 49,
   name: "Married In Vegas",
   artist: "The Vamps",
   genre: "Alternative Pop/Rock",
-  image: '/images/vegas.png'
+  image: require('../images/vegas.png')
 },
 {
   rank: 50,
   name: "hello!",
   artist: "ROLE MODEL",
   genre: "Alternative Pop",
-  image: '/images/hello.jpg'
+  image: require('../images/hello.jpg')
 }
 ]
 
