bb9587bced1f2c0650dcbe3c653a2144b635b374
Jacob Taylor <jacob.taylor0299@gmail.com>
2020-12-11 00:26:41 -0700

    .

--- front-end/src/views/SongSuggestions.vue
+++ front-end/src/views/SongSuggestions.vue
@@ -104,11 +104,15 @@ img {
 .namebox {
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
+  align-items: flex-start;
+  justify-content: center;
   width: 100%;
   height: 100%;
   background-color: #38659c;
+}
+
+.namebox h1 {
+  font-size: 28pt;
 }
 
 .buffer {
