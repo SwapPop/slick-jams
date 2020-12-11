983daec8826954fbeb55104b136a7b2de8856ea8
Jacob Taylor <jacob.taylor0299@gmail.com>
2020-12-11 00:07:51 -0700

    manage option

--- front-end/src/views/SongSuggestions.vue
+++ front-end/src/views/SongSuggestions.vue
@@ -3,6 +3,9 @@
     <div class="heading">
       <h1>your suggestions to us!</h1>
     </div>
+    <div class="links">
+      <a @click="switchManage()">manage suggestions</a>
+    </div>
     <div class="song" v-for="song in songs" :key="song._id">
       <div class="image">
         <img :src="song.image">
@@ -19,6 +22,8 @@
           </div>
         </div>
       </div>
+      <button v-show="manageCheck()">edit</button>
+      <button v-show="manageCheck()">delete</button>
     </div>
   </div>
 </template>
@@ -31,6 +36,7 @@ export default {
   name: 'songSuggestions',
   data() {
     return {
+      manage: false,
       songs: []
     }
   },
@@ -47,6 +53,12 @@ export default {
         console.log(error);
       }
     },
+    switchManage() {
+      this.manage = !this.manage;
+    },
+    manageCheck() {
+      return this.manage;
+    }
   },
 }
 </script>
@@ -117,6 +129,35 @@ img {
   align-items: center;
   height: 100%;
   width: 100%;
+}
+
+.links a {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border-radius: 12px;
+  font-weight: bold;
+  font-size: 24pt;
+  padding: 20px;
+  margin: 10px;
+  height: 50px;
+  color: #80132f;
+  background-color: #2c3e50;
+}
+
+.links a:hover {
+  background-color: #80132f55;
+}
+
+.links a.router-link-exact-active {
+  color: #8f8787;
+}
+
+.links {
+  grid-area: "links";
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 }
 
 </style>
